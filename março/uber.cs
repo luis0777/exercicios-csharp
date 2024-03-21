6809dea916883e9c5e398302ba3d141d4da5301f
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-03-21 18:46:07 -0300

    Update uber.cs

--- março/uber.cs
+++ março/uber.cs
@@ -3,7 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+public class Uber
+{
+    private string local;
+    private string destino;
+    private string valor;
+    private float pagamento;
+
+    public Uber()
+    {
+    }
+
+    public void Viajar()
+    {
+        float Carteira = 0;
+        string nome;
+        Console.WriteLine("Bem vindo ao aplicativo da Uber, qual o seu nome? ");
+        nome = Console.ReadLine();
+        Console.WriteLine($"Olá {nome}, siga o passo a passo para solicitar sua viagem. Tecle ENTER para continuar... ");
+        Console.Clear();
+        string Local, Destino;
+        float Km;
+        Console.WriteLine("Qual seu local atual?");
+        Local = Console.ReadLine();
+        Console.WriteLine("Qual o destino que gostaria de ir?");
+        Destino = Console.ReadLine();
+        Console.WriteLine("Digite a quantidade de km do seu local até o destino escolhido:");
+        Km = Convert.ToInt32(Console.ReadLine());
+        float Corrida = Km * 5;
+        Console.Clear();
+        Console.WriteLine($"Local:{Local}\nDestino:{Destino}\nValor da corrida: R${Corrida}");
+        Console.WriteLine("TECLE ENTER PARA CONTINUAR");
+        Console.ReadLine();
+
+        string Resposta;
+        Console.WriteLine("Gostaria de confirmar a corrida? Digite sim ou não");
+        Resposta = Console.ReadLine();
+
+        switch (Resposta.ToLower())
+        {
+            case "sim":
+                Console.WriteLine("Ira precisar de troco? Digite sim ou não");
+                string Troco;
+                Troco = Console.ReadLine();
+                if (Troco.ToLower() == "sim")
+                {
+                    float dinheiro;
+                    Console.WriteLine("Precisa de troco para quanto?");
+                    dinheiro = Convert.ToInt32(Console.ReadLine());
+                    float devolver = dinheiro - Corrida;
+                    Console.WriteLine($"Seu troco é R${devolver}");
+                    Console.ReadKey();
+                    int min = 10;
+
+                    while(min > 0)
+                    {
+                        Console.WriteLine($"Aguarde a chegada do seu moto, ele chega em {min} minutos");
+                        Console.ReadKey(true);
+
+                        min--;
+
+                    }
+                    Console.WriteLine("O seu motorista chegou no destino");
+                    Console.ReadKey() ;
+                }
+                else
+                {
+                    Console.WriteLine("Ok, aguarde a chegada do motorista");
+                    Console.ReadKey(true);
+
+                }
+                         break;
+
+        }
+    }
+
+}
+
+namespace LuisFelipe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uber luis = new Uber();
+            luis.Viajar();
+
+
+        }
+    }
+}
 public class Uber
 {
     private string local;
